cf9cfd273367899bef9e03f7acb8c1a6e41b7cd2
heburgosMELI <heyling.burgos@mercadolibre.com.co>
2025-01-28 15:19:15 -0500

    ✨ feat: Add a new endpoint to get a specific employee by the employee id

--- internal/repository/employee/employee.go
+++ internal/repository/employee/employee.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/luisantonisu/wave15-grupo4/internal/domain/model"
 )
 
@@ -16,6 +18,19 @@ type EmployeeRepository struct {
 	db map[int]model.Employee
 }
 
+func (r *EmployeeRepository) employeeExists(id int) bool {
+	_, exists := r.db[id]
+	return exists
+}
+
 func (r *EmployeeRepository) GetAll() (map[int]model.Employee, error) {
 	return r.db, nil
+}
+
+func (r *EmployeeRepository) GetByID(id int) (model.Employee, error) {
+	if !r.employeeExists(id) {
+		return model.Employee{}, errors.New("employee not found")
+	}
+
+	return r.db[id], nil
 }
