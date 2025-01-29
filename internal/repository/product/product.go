273abf0b171137ddea60f1b9a5069c620bc547a8
jsdzgzMELI <0fps4k@gmail.com>
2025-01-29 11:51:23 -0500

    ✨ feat: add delete product functionality and refactor product handler methods

--- internal/repository/product/product.go
+++ internal/repository/product/product.go
@@ -38,6 +38,11 @@ func (productRepository *ProductRepository) GetProductById(id int) (product mode
 }
 
 func (productRepository *ProductRepository) CreateProduct(productAtrributes *model.ProductAtrributes) (err error) {
+	for _, prod := range productRepository.db {
+		if prod.ProductAtrributes.ProductCode == productAtrributes.ProductCode {
+			return errors.New("Product already exists")
+		}
+	}
 	var newProduct model.Product
 	newProduct.ID = len(productRepository.db) + 1
 	newProduct.ProductAtrributes = *productAtrributes
@@ -45,5 +50,14 @@ func (productRepository *ProductRepository) CreateProduct(productAtrributes *mod
 		return errors.New("Product is nil")
 	}
 	productRepository.db[len(productRepository.db)+1] = newProduct
+	return nil
+}
+
+func (productRepository *ProductRepository) DeleteProduct(id int) (err error) {
+	_, ok := productRepository.db[id]
+	if !ok {
+		return errors.New("Product not found")
+	}
+	delete(productRepository.db, id)
 	return nil
 }
