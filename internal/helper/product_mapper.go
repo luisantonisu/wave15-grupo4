f872adf5481772b6145d520f026e3d2f9c9aff19
jsdzgzMELI <0fps4k@gmail.com>
2025-02-17 10:44:37 -0500

    ♻️ refactor: simplify product DTO structure by nesting ProductRequestDTO and updating related mappings

--- internal/helper/product_mapper.go
+++ internal/helper/product_mapper.go
@@ -7,41 +7,26 @@ import (
 
 func ProductToProductResponseDTO(product model.Product) dto.ProductResponseDTO {
 	data := dto.ProductResponseDTO{
-		ID:                             product.ID,
-		ProductCode:                    product.ProductAtrributes.ProductCode,
-		Description:                    product.ProductAtrributes.Description,
-		Width:                          product.ProductAtrributes.Width,
-		Height:                         product.ProductAtrributes.Height,
-		Length:                         product.ProductAtrributes.Length,
-		NetWeight:                      product.ProductAtrributes.NetWeight,
-		ExpirationRate:                 product.ProductAtrributes.ExpirationRate,
-		RecommendedFreezingTemperature: product.ProductAtrributes.RecommendedFreezingTemperature,
-		FreezingRate:                   product.ProductAtrributes.FreezingRate,
-		ProductTypeId:                  product.ProductAtrributes.ProductTypeID,
-		SellerId:                       product.ProductAtrributes.SellerID,
+		ID: product.ID,
+		ProductRequestDTO: dto.ProductRequestDTO{
+			ProductCode:                    product.ProductCode,
+			Description:                    product.Description,
+			Width:                          product.Width,
+			Height:                         product.Height,
+			Length:                         product.Length,
+			NetWeight:                      product.NetWeight,
+			ExpirationRate:                 product.ExpirationRate,
+			RecommendedFreezingTemperature: product.RecommendedFreezingTemperature,
+			FreezingRate:                   product.FreezingRate,
+			ProductTypeId:                  product.ProductTypeID,
+			SellerId:                       product.SellerID,
+		},
 	}
 	return data
 }
 
 func ProductRequestDTOToProduct(product dto.ProductRequestDTO) model.ProductAtrributes {
 	data := model.ProductAtrributes{
-		ProductCode:                    product.ProductCode,
-		Description:                    product.Description,
-		Width:                          product.Width,
-		Height:                         product.Height,
-		Length:                         product.Length,
-		NetWeight:                      product.NetWeight,
-		ExpirationRate:                 product.ExpirationRate,
-		RecommendedFreezingTemperature: product.RecommendedFreezingTemperature,
-		FreezingRate:                   product.FreezingRate,
-		ProductTypeID:                  product.ProductTypeId,
-		SellerID:                       product.SellerId,
-	}
-	return data
-}
-
-func ProductRequestDTOPtrToProductPtr(product dto.ProductRequestDTOPtr) model.ProductAtrributesPtr {
-	data := model.ProductAtrributesPtr{
 		ProductCode:                    product.ProductCode,
 		Description:                    product.Description,
 		Width:                          product.Width,
