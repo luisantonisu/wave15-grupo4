7c3e9955e440602c84ae3ac9ba3dfdea2c4459fe
jsdzgzMELI <0fps4k@gmail.com>
2025-02-21 15:35:46 -0500

    ♻️ refactor: Remove commented-out code and unused interfaces from product mock

--- internal/repository/product/product_mock.go
+++ internal/repository/product/product_mock.go
@@ -5,36 +5,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-/* func NewProductMock() *ProductMock {
-	return &ProductMock{}
-}
-
-type ProductMock struct {
-	// FuncSearchProducts is the function that proxy the SearchProducts method.
-	FuncGetProduct           func() (productMap map[int]model.Product, err error)
-	FuncGetProductByID       func(id int) (product model.Product, err error)
-	FuncGetProductRecord     func() (productRecordMap map[int]model.ProductRecordCount, err error)
-	FuncGetProductRecordByID func(id int) (productRecord model.ProductRecordCount, err error)
-	FuncCreateProduct        func(productAtrributes *model.ProductAttributes) (prod model.Product, err error)
-	FuncDeleteProduct        func(id int) (err error)
-	FuncUpdateProduct        func(id int, productAtrributes *model.ProductAttributes) (producto *model.Product, err error)
-	// Spy
-	Spy struct {
-		// SearchProducts is the number of times the SearchProducts method is called.
-		CreateProduct int
-	}
-} */
-
-/* type IProduct interface {
-	GetProduct() (productMap map[int]model.Product, err error)
-	GetProductByID(id int) (product model.Product, err error)
-	GetProductRecord() (productRecordMap map[int]model.ProductRecordCount, err error)
-	GetProductRecordByID(id int) (productRecord model.ProductRecordCount, err error)
-	CreateProduct(productAtrributes *model.ProductAtrributes) (prod model.Product, err error)
-	DeleteProduct(id int) (err error)
-	UpdateProduct(id int, productAtrributes *model.ProductAtrributes) (producto *model.Product, err error)
-} */
-
 type MockProductRepository struct {
 	mock.Mock
 }
