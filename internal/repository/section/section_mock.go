59f9509ebd12bee263c6daec06e376379877b8b1
ibzanjared <jared.peralta14@outlook.com>
2025-02-26 10:01:13 -0500

    :sparkles: feat: service test for section

--- internal/repository/section/section_mock.go
+++ internal/repository/section/section_mock.go
@@ -24,21 +24,21 @@ func (m *MockSectionRepository) GetByID(id int) (model.Section, error) {
 }
 
 func (m *MockSectionRepository) Create(section model.SectionAttributes) (model.Section, error) {
-	args := m.Called()
+	args := m.Called(section)
 	return args.Get(0).(model.Section), args.Error(1)
 }
 
 func (m *MockSectionRepository) Patch(id int, section model.SectionAttributes) (model.Section, error) {
-	args := m.Called()
+	args := m.Called(id, section)
 	return args.Get(0).(model.Section), args.Error(1)
 }
 
 func (m *MockSectionRepository) Delete(id int) error {
-	args := m.Called()
+	args := m.Called(id)
 	return args.Error(0)
 }
 
 func (m *MockSectionRepository) Report(id *int) ([]model.ReportProductsBatches, error) {
-	args := m.Called()
+	args := m.Called(id)
 	return args.Get(0).([]model.ReportProductsBatches), args.Error(1)
 }
--- internal/service/section/service_test.go
+++ internal/service/section/service_test.go
@@ -5,68 +5,171 @@ import (
 
 	"github.com/luisantonisu/wave15-grupo4/internal/domain/model"
 	repository "github.com/luisantonisu/wave15-grupo4/internal/repository/section"
-	errorHandler "github.com/luisantonisu/wave15-grupo4/pkg/error_handler"
+	eh "github.com/luisantonisu/wave15-grupo4/pkg/error_handler"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	SectionNumber      = "1"
-	CurrentTemperature = 5
-	MinimumTemperature = 5
+	CurrentTemperature = 5.5
+	MinimumTemperature = 5.5
 	CurrentCapacity    = 5
 	MinimumCapacity    = 5
 	MaximumCapacity    = 5
 	WarehouseID        = 5
 	ProductTypeID      = 5
-	MockSection        = model.Section{ID: 1,
-		SectionAttributes: model.SectionAttributes{
-			SectionNumber: &SectionNumber,
-		}}
+
+	MockSectionAtributes = model.SectionAttributes{CurrentTemperature: &CurrentTemperature, MinimumTemperature: &MinimumTemperature, CurrentCapacity: &CurrentCapacity, MinimumCapacity: &MinimumCapacity, MaximumCapacity: &MaximumCapacity, WarehouseID: &WarehouseID, ProductTypeID: &ProductTypeID}
+	MockSection          = model.Section{ID: 1, SectionAttributes: MockSectionAtributes}
+	MockSections         = []model.Section{
+		{
+			ID:                1,
+			SectionAttributes: MockSectionAtributes,
+		},
+		{
+			ID:                2,
+			SectionAttributes: MockSectionAtributes,
+		},
+	}
 )
 
 func TestSectionService_Create(t *testing.T) {
+	mockRepo := repository.NewMockRepository()
+	service := NewSectionService(mockRepo)
 
+	t.Run("Case 1: create section successfully", func(t *testing.T) {
+		t.Cleanup(func() { mockRepo.ExpectedCalls = nil })
+		mockRepo.On("Create", MockSectionAtributes).Return(MockSection, nil)
+
+		section, err := service.Create(MockSectionAtributes)
+
+		require.NoError(t, err)
+		require.NotNil(t, section)
+		require.Equal(t, MockSection, section)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("Case 2: conflict - section number id already exist", func(t *testing.T) {
+		errId := eh.GetErrAlreadyExists(eh.SECTION_NUMBER)
+		mockRepo.On("Create", MockSectionAtributes).Return(model.Section{}, errId)
+
+		section, err := service.Create(MockSectionAtributes)
+
+		require.Error(t, err)
+		require.ErrorIs(t, err, eh.ErrAlreadyExists)
+		require.Equal(t, errId, err)
+		require.Equal(t, model.Section{}, section)
+		mockRepo.AssertExpectations(t)
+	})
 }
 
 func TestSectionService_Read(t *testing.T) {
 	mockRepo := repository.NewMockRepository()
 	service := NewSectionService(mockRepo)
 
-	t.Run("Case 1: find all", func(t *testing.T) {
-		mockSections := []model.Section{
-			{
-				ID: 1,
-				SectionAttributes: model.SectionAttributes{
-					SectionNumber: &SectionNumber,
-				},
-			},
-		}
-		mockRepo.On("GetAll").Return(mockSections, nil)
+	t.Run("Case 1: get all sections successfully", func(t *testing.T) {
+		t.Cleanup(func() { mockRepo.ExpectedCalls = nil })
+		mockRepo.On("GetAll").Return(MockSections, nil)
 
 		sections, err := service.GetAll()
 
 		require.NoError(t, err)
-		require.Equal(t, mockSections, sections)
+		require.NotNil(t, sections)
+		require.Equal(t, MockSections, sections)
 		mockRepo.AssertExpectations(t)
 
 	})
 
-	t.Run("Case 2: find by id non existent", func(t *testing.T) {
-		mockRepo.On("GetByID", 2).Return(model.Section{}, errorHandler.GetErrNotFound(errorHandler.SECTION))
+	t.Run("Case 2: get all sections successfully, no sections found", func(t *testing.T) {
+		t.Cleanup(func() { mockRepo.ExpectedCalls = nil })
+		mockRepo.On("GetAll").Return([]model.Section{}, nil)
 
-		section, err := service.GetByID(2)
+		section, err := service.GetAll()
+
+		require.NoError(t, err)
+		require.Empty(t, section)
+		require.Equal(t, []model.Section{}, section)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("Case 3: find by id non existent", func(t *testing.T) {
+		errId := eh.GetErrNotFound(eh.SECTION)
+		mockRepo.On("GetByID", 3).Return(model.Section{}, errId)
+
+		section, err := service.GetByID(3)
+
 		require.Error(t, err)
+		require.ErrorIs(t, err, eh.ErrNotFound)
+		require.Equal(t, errId, err)
 		require.Equal(t, model.Section{}, section)
 		mockRepo.AssertExpectations(t)
 	})
 
-	t.Run("Case 3: find by id existent", func(t *testing.T) {
+	t.Run("Case 4: find by id existent", func(t *testing.T) {
 		mockRepo.On("GetByID", 1).Return(MockSection, nil)
 
 		section, err := service.GetByID(1)
+
 		require.NoError(t, err)
+		require.NotNil(t, section)
 		require.Equal(t, MockSection, section)
 		mockRepo.AssertExpectations(t)
 	})
 
+}
+
+func TestSectionService_Update(t *testing.T) {
+	mockRepo := repository.NewMockRepository()
+	service := NewSectionService(mockRepo)
+
+	t.Run("Case 1: update section successfully", func(t *testing.T) {
+		t.Cleanup(func() { mockRepo.ExpectedCalls = nil })
+		mockRepo.On("Patch", 1, MockSectionAtributes).Return(MockSection, nil)
+
+		section, err := service.Patch(1, MockSectionAtributes)
+
+		require.NoError(t, err)
+		require.NotNil(t, section)
+		require.Equal(t, MockSection, section)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("Case 2: not found - update section, id non existent", func(t *testing.T) {
+		errId := eh.GetErrNotFound(eh.SECTION)
+		mockRepo.On("Patch", 1, MockSectionAtributes).Return(model.Section{}, errId)
+
+		section, err := service.Patch(1, MockSectionAtributes)
+
+		require.Error(t, err)
+		require.ErrorIs(t, err, eh.ErrNotFound)
+		require.Equal(t, errId, err)
+		require.Equal(t, model.Section{}, section)
+		mockRepo.AssertExpectations(t)
+	})
+}
+
+func TestSectionService_Delete(t *testing.T) {
+	mockRepo := repository.NewMockRepository()
+	service := NewSectionService(mockRepo)
+
+	t.Run("Case 1:", func(t *testing.T) {
+		t.Cleanup(func() { mockRepo.ExpectedCalls = nil })
+		mockRepo.On("Delete", 1).Return(nil)
+
+		err := service.Delete(1)
+
+		require.NoError(t, err)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("Case 2:", func(t *testing.T) {
+		errId := eh.GetErrNotFound(eh.BUYER)
+		mockRepo.On("Delete", 1).Return(errId)
+
+		err := service.Delete(1)
+
+		require.Error(t, err)
+		require.ErrorIs(t, err, eh.ErrNotFound)
+		require.Equal(t, errId, err)
+		mockRepo.AssertExpectations(t)
+	})
 }
