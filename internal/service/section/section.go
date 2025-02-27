a3063262d5d9db127693aa27822652ab0eca49ec
ibzanjared <jared.peralta14@outlook.com>
2025-02-27 17:46:02 -0500

    :sparkles: feat: handler test finished

--- internal/service/section/section.go
+++ internal/service/section/section.go
@@ -2,18 +2,24 @@ package service
 
 import (
 	"github.com/luisantonisu/wave15-grupo4/internal/domain/model"
-	sectionRp "github.com/luisantonisu/wave15-grupo4/internal/repository/section"
+	repositoryProduct "github.com/luisantonisu/wave15-grupo4/internal/repository/product"
+	sectionRepository "github.com/luisantonisu/wave15-grupo4/internal/repository/section"
+	warehouseRepository "github.com/luisantonisu/wave15-grupo4/internal/repository/warehouse"
+	eh "github.com/luisantonisu/wave15-grupo4/pkg/error_handler"
 )
 
-func NewSectionService(sectionRp sectionRp.ISection) *SectionService {
+func NewSectionService(sectionRepository sectionRepository.ISection, repositoryProduct repositoryProduct.IProduct, warehouseRepository warehouseRepository.IWarehouse) *SectionService {
 	return &SectionService{
-		sectionRp: sectionRp,
+		sectionRp:   sectionRepository,
+		productRp:   repositoryProduct,
+		warehouseRp: warehouseRepository,
 	}
 }
 
 type SectionService struct {
-	sectionRp sectionRp.ISection
-	// productRepo repositoryProduct.IProduct
+	sectionRp   sectionRepository.ISection
+	productRp   repositoryProduct.IProduct
+	warehouseRp warehouseRepository.IWarehouse
 }
 
 func (h *SectionService) GetAll() ([]model.Section, error) {
@@ -29,6 +35,16 @@ func (h *SectionService) GetByID(id int) (model.Section, error) {
 }
 
 func (h *SectionService) Create(section model.SectionAttributes) (model.Section, error) {
+	_, err := h.warehouseRp.GetByID(*section.WarehouseID)
+	if err != nil {
+		return model.Section{}, eh.GetErrForeignKey(eh.WAREHOUSE)
+	}
+
+	_, err = h.productRp.GetProductByID(*section.ProductTypeID)
+	if err != nil {
+		return model.Section{}, eh.GetErrForeignKey(eh.PRODUCT)
+	}
+
 	newSection, err := h.sectionRp.Create(section)
 	if err != nil {
 		return model.Section{}, err
@@ -37,6 +53,20 @@ func (h *SectionService) Create(section model.SectionAttributes) (model.Section,
 }
 
 func (h *SectionService) Patch(id int, section model.SectionAttributes) (model.Section, error) {
+	if section.WarehouseID != nil {
+		_, err := h.warehouseRp.GetByID(*section.WarehouseID)
+		if err != nil {
+			return model.Section{}, eh.GetErrForeignKey(eh.WAREHOUSE)
+		}
+	}
+
+	if section.ProductTypeID != nil {
+		_, err := h.productRp.GetProductByID(*section.ProductTypeID)
+		if err != nil {
+			return model.Section{}, eh.GetErrForeignKey(eh.PRODUCT)
+		}
+	}
+
 	updateSection, err := h.sectionRp.Patch(id, section)
 	if err != nil {
 		return model.Section{}, err
